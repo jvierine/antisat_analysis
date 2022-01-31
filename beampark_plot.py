4a6d3047373e4c456c389112cd3d007d6228c8aa
danielk <daniel.kastinen@irf.se>
2022-01-31 13:30:18 +0100

    added size distribution plots and better radar selection system

--- beampark_plot.py
+++ beampark_plot.py
@@ -3,12 +3,7 @@
 '''
 Example execution 
 
-RUN:
-
-PLOT:
-
-./beampark_elevation_doppler.py plot cache/**/az*.h5 -o ./plots/
-./beampark_elevation_doppler.py plot -d /home/danielk/data/spade/beamparks/uhf/2021.11.23.h5 -o ./plots/ -- ./cache/eiscat_uhf/az90.0_el75.0.h5
+./beampark_plot.py -o ./plots/test/ -r "EISCAT UHF" ~/data/spade/beamparks/uhf/2021.11.23.h5
 
 '''
 import sys
@@ -23,8 +18,23 @@ from astropy.time import Time, TimeDelta
 import matplotlib.dates as mdates
 import h5py
 
+import sorts
 
 HERE = Path(__file__).parent
+
+
+radar_aliases = {
+    'eiscat3d': ['eiscat3d', 'EISCAT 3D', 'e3d', 'EISCAT_3D', 'e 3d', 'e_3d'],
+    'eiscat_uhf': ['eiscat_uhf', 'EISCAT UHF', 'UHF', 'e_uhf', 'e uhf'],
+    'eiscat_esr': ['eiscat_esr', 'EISCAT Svalbard Radar', 'ESR', 'e_sr', 'e sr'],
+}
+radar_verbose_names = {
+    'eiscat3d': 'EISCAT 3D',
+    'eiscat_uhf': 'EISCAT UHF',
+    'eiscat_esr': 'EISCAT Svalbard Radar',
+}
+TX_IND = 0
+RX_IND = 0
 
 
 def load_data(file):
@@ -113,10 +123,10 @@ def time_name_formatting(t, radar):
     return radar_escaped, t_dt, start_time, end_time, fig_name
 
 
-def plot_statistics(data, radar, save_path=None, size=(10, 8)):
+def plot_statistics(data, radar_verbose, save_path=None, size=(10, 8)):
 
     t, r, v, snr, dur, diam = data
-    radar_escaped, t_dt, start_time, end_time, fig_name = time_name_formatting(t, radar)
+    radar_escaped, t_dt, start_time, end_time, fig_name = time_name_formatting(t, radar_verbose)
 
     filt = np.logical_and(v > -3, v < 3)
 
@@ -145,14 +155,14 @@ def plot_statistics(data, radar, save_path=None, size=(10, 8)):
     axes[1, 1].set_xlabel("Diameter [log10(cm)]")
     axes[1, 1].set_ylabel("Detections")
 
-    fig.suptitle(f'Observation statistics at {radar} {start_time.iso}')
+    fig.suptitle(f'Observation statistics at {radar_verbose} {start_time.iso}')
     save_fig('stat', save_path, fig, radar_escaped, fig_name)
 
 
-def plot_detections(data, radar, save_path=None, size=(10, 8)):
+def plot_detections(data, radar_verbose, save_path=None, size=(10, 8)):
 
     t, r, v, snr, dur, diam = data
-    radar_escaped, t_dt, start_time, end_time, fig_name = time_name_formatting(t, radar)
+    radar_escaped, t_dt, start_time, end_time, fig_name = time_name_formatting(t, radar_verbose)
 
     bin_size = 1800.0
     bin_centers, hist = count_hist(t, start_time, end_time, bin_size=bin_size)
@@ -173,7 +183,7 @@ def plot_detections(data, radar, save_path=None, size=(10, 8)):
 
     set_mdates(axes[2])
 
-    fig.suptitle(f'Observations at {radar} {start_time.iso}')
+    fig.suptitle(f'Observations at {radar_verbose} {start_time.iso}')
     save_fig('obs', save_path, fig, radar_escaped, fig_name)
 
     fig, ax = plt.subplots(figsize=size)
@@ -181,7 +191,7 @@ def plot_detections(data, radar, save_path=None, size=(10, 8)):
     ax.set_ylabel("Detections [per 30 min]")
     ax.set_xlabel('Time')
     set_mdates(ax)
-    fig.suptitle(f'Observations at {radar} {start_time.iso}')
+    fig.suptitle(f'Observations at {radar_verbose} {start_time.iso}')
     save_fig('counts', save_path, fig, radar_escaped, fig_name)
 
     fig, axes = plt.subplots(2, 1, sharex='all', figsize=size)
@@ -195,7 +205,7 @@ def plot_detections(data, radar, save_path=None, size=(10, 8)):
     axes[1].set_ylim([-3, 3])
 
     set_mdates(axes[1])
-    fig.suptitle(f'Observations at {radar} {start_time.iso}')
+    fig.suptitle(f'Observations at {radar_verbose} {start_time.iso}')
     save_fig('obs_vs_t', save_path, fig, radar_escaped, fig_name)
 
     fig, ax = plt.subplots(figsize=size)
@@ -204,8 +214,55 @@ def plot_detections(data, radar, save_path=None, size=(10, 8)):
     ax.set_ylabel('Range-rate [km/s]')
     ax.set_xlim([200, 2000])
     ax.set_ylim([-3, 3])
-    fig.suptitle(f'Observations at {radar} {start_time.iso}')
+    fig.suptitle(f'Observations at {radar_verbose} {start_time.iso}')
     save_fig('range_range_rate', save_path, fig, radar_escaped, fig_name)
+
+
+def plot_size_distribution(data, radar_verbose, tx, rx, save_path=None, size=(10, 8)):
+    
+    t, r, v, snr, dur, diam = data
+    radar_escaped, t_dt, start_time, end_time, fig_name = time_name_formatting(t, radar_verbose)
+
+    # Assume main-lobe peak
+    G = tx.beam.gain(tx.beam.pointing)
+
+    r_vec = np.linspace(np.min(r), np.max(r), num=1000)
+
+    min_diam = sorts.signals.hard_target_diameter(
+        gain_tx=G,
+        gain_rx=G,
+        wavelength=tx.beam.wavelength,
+        power_tx=tx.power,
+        range_tx_m=r_vec*1e3,
+        range_rx_m=r_vec*1e3,
+        snr=33,
+        bandwidth=tx.coh_int_bandwidth,
+        rx_noise_temp=rx.noise,
+        radar_albedo=1,
+    )
+    # m -> cm
+    min_diam *= 100
+
+    fig, ax = plt.subplots(figsize=size)
+    ax.scatter(r, np.log10(diam), 4, label='Detections')
+    ax.plot(r_vec, np.log10(min_diam), '-r', label='Minimum detecable diameter [15.185 dB SNR]')
+
+    ax.set_xlabel('Range [km]')
+    ax.set_ylabel('Diameter [log10(cm)]')
+    ax.legend()
+    fig.suptitle(f'Observations at {radar_verbose} {start_time.iso}')
+    save_fig('min_diam_dist', save_path, fig, radar_escaped, fig_name)
+
+    fig, ax = plt.subplots(figsize=size)
+    sc = ax.scatter(v, np.log10(diam), 4, 10*np.log10(snr), label='Detections')
+
+    ax.set_xlabel('Range rate [km/s]')
+    ax.set_ylabel('Diameter [log10(cm)]')
+    cb = fig.colorbar(sc, ax=ax)
+    cb.set_label('SNR [dB]')
+    ax.legend()
+    fig.suptitle(f'Observations at {radar_verbose} {start_time.iso}')
+    save_fig('dr_diam_snr', save_path, fig, radar_escaped, fig_name)
 
 
 def main(input_args=None):
@@ -231,6 +288,20 @@ def main(input_args=None):
     else:
         args = parser.parse_args(input_args)
 
+    radar = None
+    for key in radar_aliases:
+        for rname in radar_aliases[key]:
+            if args.radar.lower() == rname.lower():
+                radar = key
+                break
+    if radar is None:
+        radar_options = '\n'.join([f'{key}:\n  ' + ', '.join(radar_aliases[key]) for key in radar_aliases])
+        raise ValueError(f'Radar "{args.radar}" not found, options are: \n{radar_options}')
+
+    radar_verbose = radar_verbose_names[radar]
+    radar_instance = getattr(sorts.radars, radar)
+    tx, rx = radar_instance.tx[TX_IND], radar_instance.rx[RX_IND]
+
     if args.dark:
         plt.style.use('dark_background')
     else:
@@ -252,13 +323,21 @@ def main(input_args=None):
         data = load_data(file)
         plot_statistics(
             data, 
-            radar = args.radar, 
+            radar_verbose = radar_verbose, 
             save_path=output_path, 
             size=(args.plot_width, args.plot_height),
         )
         plot_detections(
             data, 
-            radar = args.radar, 
+            radar_verbose = radar_verbose, 
+            save_path=output_path, 
+            size=(args.plot_width, args.plot_height),
+        )
+        plot_size_distribution(
+            data, 
+            radar_verbose = radar_verbose, 
+            tx=tx,
+            rx=rx,
             save_path=output_path, 
             size=(args.plot_width, args.plot_height),
         )
