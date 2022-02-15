f321fd2ad500305a791a78eef69ec91b852c481a
danielk <daniel.kastinen@irf.se>
2022-02-15 11:13:51 +0100

    discosweb cli working

--- discosweb_download.py
+++ discosweb_download.py
@@ -1,27 +1,59 @@
 #!/usr/bin/env python
 import sys
-import re
 import getpass
 import argparse
-import pathlib
+import json
 import subprocess
-from pprint import pprint
 import requests
+from pprint import pprint
 
 URL = 'https://discosweb.esoc.esa.int'
 
+_ID_MAP = {
+    'NORAD': 'satno',
+    'COSPAR': 'cosparId',
+    'DISCOS': 'id',
+}
+
+
 def main(input_args=None):
-    parser = argparse.ArgumentParser(description='Download size information from discosweb')
-    parser.add_argument('output', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser = argparse.ArgumentParser(
+        description='Download size information from discosweb',
+    )
+    parser.add_argument(
+        'object_id', metavar='ID', type=str, 
+        help='Input ID, ID variant can be set with "--type"',
+    )
+    parser.add_argument(
+        '-t', '--type', type=str, default='NORAD',
+        choices = ['NORAD', 'COSPAR', 'DISCOS'],
+        help='Input ID, ID variant can be set with "--type"',
+    )
+    parser.add_argument(
+        'output', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+        help = 'Path to file where output should be written.'
+    )
     parser.add_argument('--secret-tool-key', '-k', nargs=1)
-    parser.add_argument('--credentials', '-c', nargs=1, help='File containing username and password for space-track.org')
-    parser.add_argument('--name', '-n', default=None, help='Name of the object to match with the "like" operator')
+    parser.add_argument(
+        '--credentials', '-c', nargs=1, 
+        help='File containing DISCOSweb token',
+    )
+    parser.add_argument(
+        '--name', '-n', default=None, 
+        help='Name of the object to match with the "like" operator',
+    )
 
     if input_args is None:
         args = parser.parse_args()
     else:
         args = parser.parse_args(input_args)
 
+    if args.type == 'NORAD':
+        if args.object_id[-1].isalpha():
+            args.object_id = args.object_id[:-1]
+    elif args.type == 'COSPAR':
+        args.object_id = f"'{args.object_id}'"
+    
     if args.secret_tool_key is not None:
         res = subprocess.run(
             ['secret-tool', 'lookup', 'token'] + args.secret_tool_key, 
@@ -33,6 +65,9 @@ def main(input_args=None):
     else:
         token = getpass.getpass("API token for:")
 
+    filt_str = f"{_ID_MAP[args.type]}={args.object_id}"
+    print(f'Fetching data for "{filt_str}"')
+
     response = requests.get(
         f'{URL}/api/objects',
         headers={
@@ -40,15 +75,37 @@ def main(input_args=None):
             'DiscosWeb-Api-Version': '2',
         },
         params={
-            'filter': "eq(objectClass,Payload)&gt(reentry.epoch,epoch:'2020-01-01')",
-            'sort': '-reentry.epoch',
+            'filter': filt_str,
         },
     )
 
+    # Mass
+    # 3964.32 kg
+    # Shape
+    # Cyl
+    # Width
+    # –
+    # Height
+    # 28 m
+    # Depth
+    # –
+    # Diameter
+    # 2.6 m
+    # Span
+    # 28 m
+    # Max. cross section
+    # 72.9933461154505 m²
+    # Min. cross section
+    # 5.30929158456675 m²
+    # Avg. cross section
+    # 59.8316320876176 m²
+
     doc = response.json()
     if response.ok:
-        pprint(doc['data'])
+        print(f'{len(doc)} Entries found...')
+        json.dump(doc['data'], args.output, indent=2)
     else:
+        print('Error...')
         pprint(doc['errors'])
 
 
