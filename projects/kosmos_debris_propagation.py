13d55afcd4e4dbb3f5cd47b1c45dc5d9643ea38d
danielk <daniel.kastinen@irf.se>
2022-03-14 09:27:46 +0100

    better propagation with ffmpg moveie

--- projects/kosmos_debris_propagation.py
+++ projects/kosmos_debris_propagation.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
-'''
-Orekit propagator usage
-================================
-'''
 from pathlib import Path
-import time
+import subprocess
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,10 +21,31 @@ except ImportError:
     comm = COMM_WORLD()
 
 fragments = 1000
-target_t = 3600.0*24*7
+target_t = 3600.0*24*7*12
+snapshot_interval = 3600.0*24
 log10d_mu = -1.1
 log10d_std = 0.1
 delta_v_std = 200.0  # m/s
+clobber = False
+np.random.seed(289784)
+
+settings = dict(
+    in_frame='TEME',
+    out_frame='TEME',
+    solar_activity_strength='WEAK',
+    integrator='DormandPrince853',
+    min_step=0.001,
+    max_step=240.0,
+    position_tolerance=20.0,
+    earth_gravity='HolmesFeatherstone',
+    gravity_order=(10, 10),
+    solarsystem_perturbers=['Moon', 'Sun'],
+    drag_force=True,
+    atmosphere='DTM2000',
+    radiation_pressure=True,
+    solar_activity='Marshall',
+    constants_source='WGS84',
+)
 
 HERE = Path(__file__).parent.resolve()
 OUTPUT = HERE / 'output' / 'russian_asat'
@@ -37,7 +54,9 @@ print(f'Using {OUTPUT} as output')
 tle_file = OUTPUT / 'kosmos-1408-2021-11-15.tle'
 orekit_data = OUTPUT / 'orekit-data-master.zip'
 cache_file = OUTPUT / 'kosmos-1408_orekit_propagated.npy'
-
+anim_file = OUTPUT / 'kosmos-1408_orekit_propagated.avi'
+plot_folder = OUTPUT / 'kosmos-1408_orekit_propagated'
+plot_folder.mkdir(exist_ok=True)
 
 def run_orekit(
                 target_t, 
@@ -49,26 +68,32 @@ def run_orekit(
                 pbar,
             ):
 
-    out_states = np.empty((6, len(worker_inds)), dtype=np.float64)
+    out_states = np.empty(
+        (6, len(worker_inds), len(target_t)), 
+        dtype=np.float64,
+    )
     cnt = 0
     for ind in worker_inds:
         try:
-            out_states[:, cnt] = prop.propagate(
+            _st = prop.propagate(
                 target_t, states0[:, ind], 
                 epoch=mjd0, A=A[ind], C_R=1.0, C_D=1.0,
-            )[:, 0]
+            )
+            out_states[:, cnt, :] = _st
         except orekit.JavaError:
-            out_states[:, cnt] = np.nan
+            out_states[:, cnt, :] = np.nan
         cnt += 1
         pbar.update(1)
 
     return out_states
 
 
-def get_future_kepler_dist():
+def get_future_orb_dist():
 
     if not orekit_data.is_file():
-        sorts.propagator.Orekit.download_quickstart_data(orekit_data, verbose=True)
+        sorts.propagator.Orekit.download_quickstart_data(
+            orekit_data, verbose=True
+        )
 
     pop = sorts.population.tle_catalog(tle_file, kepler=True)
     pop.unique()
@@ -88,8 +113,8 @@ def get_future_kepler_dist():
     states0 = np.empty((6, fragments), dtype=np.float64)
     states0[:3, :] = state0[:3, None]
     states0[3:, :] = state0[3:, None] + np.random.randn(3, fragments)*delta_v_std
-
-    states = np.empty_like(states0)
+    t_vec = np.arange(0, target_t, snapshot_interval, dtype=np.float64)
+    states = np.empty((6, fragments, len(t_vec)), dtype=np.float64)
 
     inds = list(range(fragments))
     step = int(fragments/comm.size + 1)
@@ -102,29 +127,14 @@ def get_future_kepler_dist():
 
     prop = sorts.propagator.Orekit(
         orekit_data=orekit_data,
-        settings=dict(
-            in_frame='TEME',
-            out_frame='TEME',
-            solar_activity_strength='WEAK',
-            integrator='DormandPrince853',
-            min_step=0.001,
-            max_step=120.0,
-            position_tolerance=10.0,
-            earth_gravity='HolmesFeatherstone',
-            gravity_order=(10, 10),
-            solarsystem_perturbers=['Moon', 'Sun'],
-            drag_force=True,
-            atmosphere='DTM2000',
-            radiation_pressure=True,
-            solar_activity='Marshall',
-            constants_source='WGS84',
-        )
+        settings=settings,
     )
 
     pbar = tqdm(total=len(all_inds[comm.rank]), position=comm.rank)
     worker_inds = all_inds[comm.rank]
+    
     out_states = run_orekit(
-        target_t, 
+        t_vec, 
         states0, 
         mjd0, 
         A,
@@ -132,12 +142,12 @@ def get_future_kepler_dist():
         prop,
         pbar,
     )
-    states[:, worker_inds] = out_states
+    states[:, worker_inds, :] = out_states
     if comm.size > 1:
         if comm.rank == 0:
             for thr_id in range(comm.size):
                 if thr_id != 0:
-                    states[:, all_inds[thr_id]] = comm.recv(
+                    states[:, all_inds[thr_id], :] = comm.recv(
                         source=thr_id, 
                         tag=thr_id,
                     )
@@ -147,17 +157,44 @@ def get_future_kepler_dist():
     
     pbar.close()
 
-    orbs = pyorb.Orbit(M0=pyorb.M_earth, degrees=True, num=fragments)
-    orbs.cartesian = states
-
-    return orbs.kepler
+    return states
 
 
-if cache_file.is_file():
-    kepler = np.load(cache_file)
+if cache_file.is_file() and not clobber:
+    states = np.load(cache_file)
 else:
-    kepler = get_future_kepler_dist()
-    np.save(cache_file, kepler)
+    states = get_future_orb_dist()
+    if comm.rank == 0:
+        np.save(cache_file, states)
 
-sorts.plotting.kepler_scatter(kepler.T, axis_labels='earth-orbit')
-plt.show()
+t_vec = np.arange(0, target_t, snapshot_interval, dtype=np.float64)/(3600.0*24)
+
+for ind in range(states.shape[2]):
+    orbs = pyorb.Orbit(M0=pyorb.M_earth, degrees=True, num=fragments)
+    orbs.cartesian = states[:, :, ind]
+    
+    fig, ax = sorts.plotting.kepler_scatter(
+        orbs.kepler.T, 
+        axis_labels='earth-orbit',
+        title=f'KOSMOS-1408 DEB proxy-distribution t={t_vec[ind]:.1f} days',
+        limits=[
+            (1, 1.3),
+            (0, 0.2),
+            (60, 100),
+            (0, 360),
+            (0, 360),
+            (0, 360),
+        ],
+    )
+    fig.savefig(plot_folder / f't_int{ind}.png', bbox_inches='tight')
+    plt.close(fig)
+
+try:
+    subprocess.check_call(
+        f'ffmpeg -start_number 0 -r 6 -i t_int%d.png -c:v ffv1 {str(anim_file.resolve())}', 
+        cwd=str(plot_folder.resolve()),
+        shell=True,
+    )
+except subprocess.CalledProcessError as e:
+    print(e)
+    print('Could not create movie from animation frames... probably ffmpeg is missing')
